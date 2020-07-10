4406bef181344a4c21a803ebbe01386bc9175d98
huaiyongtai <huaiyongtai@gmail.com>
2020-07-10 16:57:01 +0800

    auto sync

--- yield_test.php
+++ yield_test.php
@@ -14,11 +14,18 @@ function usageMem()
 
 
 function gen() {
+    echo __LINE__;
+    exit;
     yield 1;
 }
 
 $stime = microTimeFloat();
 $smem  = usageMem();
+
+
+$gen = gen();
+var_dump($gen); exit;
+
 
 echo $smem.PHP_EOL;
 echo $smem / 1024 / 1024;
