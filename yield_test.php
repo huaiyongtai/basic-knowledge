b327949e67e9fd0c444bb3ed63094f1989b2ad8d
huaiyongtai <huaiyongtai@gmail.com>
2020-07-10 17:20:01 +0800

    auto sync

--- yield_test.php
+++ yield_test.php
@@ -16,6 +16,7 @@ function usageMem()
 function gen($a) {
     echo __LINE__.":".__FUNCTION__.":$a".PHP_EOL;
     exit;
+    return $a;
     yield ++$a;
     echo __LINE__.":".__FUNCTION__.":$a".PHP_EOL;
     yield ++$a;
