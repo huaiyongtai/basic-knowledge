d2216ff6b997bd9b6e68b9ae6e3d3a181d5192cd
huaiyongtai <huaiyongtai@gmail.com>
2020-07-10 12:59:01 +0800

    auto sync

--- yield_test.php
+++ yield_test.php
@@ -1,5 +1,11 @@
 <?php
 
-$stime = time();
+
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+$stime = microtime_float();
 
 echo $stime;
